76f8086eb789941b007f56e0670d8eb4f903d5d2
Sanskar-12 <sanskarv2004@gmail.com>
2024-06-29 17:19:24 +0530

    notification panel

--- src/app/(main)/agency/[agencyId]/layout.tsx
+++ src/app/(main)/agency/[agencyId]/layout.tsx
@@ -1,7 +1,13 @@
+import BlurPage from "@/components/blur-page/blur-page";
+import InfoBar from "@/components/info-bar/info-bar";
 import Sidebar from "@/components/sidebar/sidebar";
 import Unauthorized from "@/components/unauthorised/unauthorised";
-import { getNotificationAndUser, verifyAndAcceptInvitation } from "@/lib/queries";
+import {
+  getNotificationAndUser,
+  verifyAndAcceptInvitation,
+} from "@/lib/queries";
 import { currentUser } from "@clerk/nextjs";
+import { Role } from "@prisma/client";
 import { redirect } from "next/navigation";
 import { ReactNode } from "react";
 
@@ -24,27 +30,31 @@ const LayoutPage = async ({ children, params }: LayoutProps) => {
     return redirect("/");
   }
   if (
-    user.privateMetadata.role !== 'AGENCY_OWNER' &&
-    user.privateMetadata.role !== 'AGENCY_ADMIN'
+    user.privateMetadata.role !== "AGENCY_OWNER" &&
+    user.privateMetadata.role !== "AGENCY_ADMIN"
   )
-    return <Unauthorized />
+    return <Unauthorized />;
+
+  let allNotification: any = [];
+
+  const notifications = await getNotificationAndUser(agencyId);
+  if (notifications) allNotification = notifications;
 
 
-    let allNotification:any=[]
-
-    const notifications=await getNotificationAndUser(agencyId)
-    if(notifications) allNotification=notifications
-
-
-  return <div className="h-screen overflow-hidden">
-    <Sidebar
-    id={params.agencyId}
-    type="agency"
-    />
-    <div className="md:pl-[300px]">
-      {children}
+  return (
+    <div className="h-screen overflow-hidden">
+      <Sidebar id={params.agencyId} type="agency" />
+      <div className="md:pl-[300px]">
+        <InfoBar
+          notifications={allNotification}
+          role={allNotification?.User?.role}
+        />
+        <div className="relative">
+          <BlurPage>{children}</BlurPage>
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default LayoutPage;
