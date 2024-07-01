674ba8b277eb7760f5e7d2245ee54bb8ad93d0bb
Sanskar-12 <sanskarv2004@gmail.com>
2024-07-01 14:46:32 +0530

    launchpad page

--- src/app/(main)/agency/[agencyId]/launchpad/page.tsx
+++ src/app/(main)/agency/[agencyId]/launchpad/page.tsx
@@ -1,8 +1,105 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { db } from "@/lib/db";
+import { CheckCircleIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
 
-const LauchPadPage = () => {
-  return (
-    <div>LauchPadPage</div>
-  )
+interface LauchPadPageProps {
+  params: {
+    agencyId: string;
+  };
+  searchParams: {
+    code: string;
+  };
 }
 
-export default LauchPadPage
+const LauchPadPage = async ({
+  params: { agencyId },
+  searchParams: { code },
+}: LauchPadPageProps) => {
+  const agencyDetails = await db.agency.findUnique({
+    where: {
+      id: agencyId,
+    },
+  });
+
+  if (!agencyDetails) return;
+
+  const allDetailsExist =
+    agencyDetails.address &&
+    agencyDetails.address &&
+    agencyDetails.agencyLogo &&
+    agencyDetails.city &&
+    agencyDetails.companyEmail &&
+    agencyDetails.companyPhone &&
+    agencyDetails.country &&
+    agencyDetails.name &&
+    agencyDetails.state &&
+    agencyDetails.zipCode;
+
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-full h-full max-w-[800px]">
+        <Card className="border-none">
+          <CardHeader>
+            <CardTitle>Lets get started!</CardTitle>
+            <CardDescription>
+              Follow the steps below to get your account setup
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="flex flex-col gap-4">
+            <div className="flex justify-between items-center w-full border p-4 rounded-lg gap-2">
+              <div className="flex md:items-center gap-4 flex-col md:!flex-row">
+                <Image
+                  src="/stripelogo.png"
+                  alt="app logo"
+                  height={80}
+                  width={80}
+                  className="rounded-md object-contain"
+                />
+                <p>
+                  Connect your stripe account to accept payments and see your
+                  dashboard.
+                </p>
+              </div>
+            </div>
+            <div className="flex justify-between items-center w-full border p-4 rounded-lg gap-2">
+              <div className="flex md:items-center gap-4 flex-col md:!flex-row">
+                <Image
+                  src={agencyDetails.agencyLogo}
+                  alt="app logo"
+                  height={80}
+                  width={80}
+                  className="rounded-md object-contain"
+                />
+                <p>Fill in all your bussiness details.</p>
+              </div>
+              {allDetailsExist ? (
+                <CheckCircleIcon
+                  size={50}
+                  className="text-primary p-2 flex-shrink-0"
+                />
+              ) : (
+                <Link
+                  href={`/agency/${agencyId}/settings`}
+                  className="bg-primary py-2 px-4 rounded-md text-white"
+                >
+                  Start
+                </Link>
+              )}
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+};
+
+export default LauchPadPage;
