91b087a0f55971828b7605ac5a5a05fd21fc5f88
Sanskar-12 <sanskarv2004@gmail.com>
2024-07-03 13:28:04 +0530

    subaccount launchpad

--- src/app/(main)/subaccount/[subaccountId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/page.tsx
@@ -1,76 +1,9 @@
-import InfoBar from "@/components/info-bar/info-bar";
-import Sidebar from "@/components/sidebar/sidebar";
-import Unauthorized from "@/components/unauthorised/unauthorised";
-import {
-  getAuthUserDetails,
-  getNotificationAndUser,
-  verifyAndAcceptInvitation,
-} from "@/lib/queries";
-import { currentUser } from "@clerk/nextjs";
-import { Role } from "@prisma/client";
-import { redirect } from "next/navigation";
-import { ReactNode } from "react";
+import React from 'react'
 
-interface SubAccountIdPageProps {
-  children: ReactNode;
-  params: {
-    subaccountId: string;
-  };
+const SubAccountIdPage = () => {
+  return (
+    <div>SubAccountIdPage</div>
+  )
 }
 
-const SubAccountIdPage = async ({
-  children,
-  params: { subaccountId },
-}: SubAccountIdPageProps) => {
-  const agencyId = await verifyAndAcceptInvitation();
-  if (!agencyId) return <Unauthorized />;
-
-  const user = await currentUser();
-  if (!user) return redirect("/");
-
-  let notifications: any = [];
-
-  if (!user.privateMetadata.role) {
-    return <Unauthorized />;
-  } else {
-    const allPermission = await getAuthUserDetails();
-    const hasPermission = allPermission?.Permissions.find(
-      (permission) =>
-        permission.access && permission.subAccountId === subaccountId
-    );
-
-    if (!hasPermission) return <Unauthorized />;
-
-    const allNotifications = await getNotificationAndUser(agencyId);
-
-    if (
-      user.privateMetadata.role === "AGENCY_ADMIN" ||
-      user.privateMetadata.role === "AGENCY_OWNER"
-    ) {
-      notifications = allNotifications;
-    } else {
-      const filteredNoti = allNotifications?.filter(
-        (item) => item.subAccountId === subaccountId
-      );
-      if (filteredNoti) notifications = filteredNoti;
-    }
-  }
-
-  return  <div className="h-screen overflow-hidden">
-  <Sidebar
-    id={subaccountId}
-    type="subaccount"
-  />
-
-  <div className="md:pl-[300px]">
-    <InfoBar
-      notifications={notifications}
-      role={user.privateMetadata.role as Role}
-      subAccountId={subaccountId as string}
-    />
-    <div className="relative">{children}</div>
-  </div>
-</div>
-};
-
-export default SubAccountIdPage;
+export default SubAccountIdPage
