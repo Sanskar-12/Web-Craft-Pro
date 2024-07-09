8515a4c25e06d562e946ed1f842f3480f31e32f4
Sanskar-12 <sanskarv2004@gmail.com>
2024-07-09 21:45:55 +0530

    pipeline view

--- src/components/pipeline-view/pipeline-view.tsx
+++ src/components/pipeline-view/pipeline-view.tsx
@@ -10,7 +10,7 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 import { DragDropContext, DropResult, Droppable } from "react-beautiful-dnd";
 import { Button } from "../ui/button";
-import { Plus } from "lucide-react";
+import { Flag, Plus } from "lucide-react";
 import CustomModal from "../custommodal/custom-modal";
 import LaneForm from "../forms/lane-form";
 
@@ -60,6 +60,44 @@ const PipelineView = ({
             Create Lane
           </Button>
         </div>
+        <Droppable
+          droppableId="lanes"
+          type="lane"
+          direction="horizontal"
+          key="lanes"
+        >
+          {
+            (provided)=>(
+              <div className="flex items-center gap-x-2 overflow-scroll" {...provided.droppableProps}
+              ref={provided.innerRef}
+              >
+                <div className="flex mt-4">
+                  {
+                    allLanes.map((lane,index)=>(
+                      <div key={index}>
+                        {/* <PipelineLane/> */}
+                      </div>
+                    ))
+                  }
+                  {provided.placeholder}
+                </div>
+              </div>
+            )
+          }
+        </Droppable>
+        {
+          allLanes.length==0 && (
+            <div className="flex items-center justify-center w-full flex-col">
+              <div className="opacity-100">
+                <Flag
+                    width="100%"
+                    height="100%"
+                    className="text-muted-foreground"
+                />
+              </div>
+            </div>
+          )
+        }
       </div>
     </DragDropContext>
   );
