4cee698481ae4c1dc464ed783af115cce1d46be1
Sanskar-12 <sanskarv2004@gmail.com>
2024-07-22 19:34:40 +0530

    payment form

--- src/components/forms/subscription-form/subscription-form-wrapper.tsx
+++ src/components/forms/subscription-form/subscription-form-wrapper.tsx
@@ -1,12 +1,19 @@
 "use client";
 
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
 import { toast } from "@/components/ui/use-toast";
+import { pricingCards } from "@/lib/constants";
 import { useModal } from "@/providers/modal-provider";
 import { Plan } from "@prisma/client";
 import { StripeElementsOptions } from "@stripe/stripe-js";
 import axios from "axios";
+import { Elements } from '@stripe/react-stripe-js'
+import clsx from "clsx";
 import { useRouter } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
+import { getStripe } from "@/lib/stripe/stripe-client";
+import Loading from "@/components/loading/loading";
+import SubscriptionForm from ".";
 
 interface SubscriptionFormWrapperProps {
   customerId: string;
@@ -73,7 +80,51 @@ const SubscriptionFormWrapper = ({
     createSecret();
   }, [data,selectedPriceId,customerId]);
 
-  return <div>SubscriptionFormWrapper</div>;
+  return <div className="border-none transition-all">
+    <div className="flex flex-col gap-4">
+      {
+        data?.plans?.plans.map((price)=>(
+          <Card 
+            onClick={()=>setSelectedPriceId(price.id as Plan)}
+            key={price.id}
+            className={clsx('relative cursor-pointer transition-all', {
+              'border-primary': selectedPriceId === price.id,
+            })}
+          >
+            <CardHeader>
+              <CardTitle>
+                ${price.unit_amount ? price.unit_amount/100 : "0"}
+                <p className="text-sm text-muted-foreground">
+                  {price.nickname}
+                </p>
+                <p className="text-sm text-muted-foreground">
+                  {pricingCards.find((p)=>p.priceId===price.id)?.description}
+                </p>
+              </CardTitle>
+            </CardHeader>
+            {selectedPriceId === price.id && (
+              <div className="w-2 h-2 bg-emerald-500 rounded-full absolute top-4 right-4" />
+            )}
+          </Card>
+        ))
+      }
+      {
+        options.clientSecret && !planExists && (
+          <>
+            <h1 className="text-xl">Payment Method</h1>
+            <Elements stripe={getStripe()} options={options}>
+              <SubscriptionForm selectedPriceId={selectedPriceId}/>
+            </Elements>
+          </>
+        )
+      }
+       {!options.clientSecret && selectedPriceId && (
+          <div className="flex items-center justify-center w-full h-40">
+            <Loading />
+          </div>
+        )}
+    </div>
+  </div>;
 };
 
 export default SubscriptionFormWrapper;
