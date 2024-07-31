b405f3fb25ed310952b3c8895758eb14f7eb25c3
Sanskar-12 <sanskarv2004@gmail.com>
2024-07-31 20:21:25 +0530

    editor context made

--- src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
+++ src/app/(main)/subaccount/[subaccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
@@ -1,8 +1,43 @@
-import React from 'react'
+import { db } from "@/lib/db"
+import EditorProvider from "@/providers/editor/editor-providers"
+import { redirect } from "next/navigation"
 
-const EditorFunnelPageId = () => {
+interface EditorFunnelPageId {
+  params:{
+    subaccountId:string,
+    funnelId:string,
+    funnelPageId:string
+  }
+}
+
+const EditorFunnelPageId =async ({params:{
+  subaccountId,
+  funnelId,
+  funnelPageId
+}}:EditorFunnelPageId) => {
+
+  const funnelPageDetails=await db.funnelPage.findFirst({
+    where:{
+      id:funnelPageId
+    }
+  })
+
+  if (!funnelPageDetails) {
+    return redirect(
+      `/subaccount/${subaccountId}/funnels/${funnelId}`
+    )
+  }
+
   return (
-    <div>EditorFunnelPageId</div>
+    <div className="fixed top-0 bottom-0 left-0 right-0 z-[20] bg-background overflow-hidden">
+      <EditorProvider
+        subaccountId={subaccountId}
+        funnelId={funnelId}
+        pageDetails={funnelPageDetails}
+      > 
+        
+      </EditorProvider>
+    </div>
   )
 }
 
