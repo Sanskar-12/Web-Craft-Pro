2b33163750d69510929c25c5075e95cfb76b0289
Sanskar-12 <sanskarv2004@gmail.com>
2024-08-11 11:11:02 +0530

    bug fixed

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -21,33 +22,35 @@ export default async function Home() {
 
   return (
     <>
-      <section className="h-full w-full md:pt-44 mt-[-70px] relative flex items-center justify-center flex-col ">
-
+      <section className="h-full w-full pt-36 relative flex items-center justify-center flex-col ">
+        {/* Grid */}
         <div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#161616_1px,transparent_1px),linear-gradient(to_bottom,#161616_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_110%)] -z-10" />
-
-        <p className="text-center">Run your agency, in one place</p>
+        <p className="text-center">
+          Manage Your Entire Agency from One Central Hub
+        </p>
         <div className="bg-gradient-to-r from-primary to-secondary-foreground text-transparent bg-clip-text relative">
-          <h1 className="text-9xl font-bold text-center md:text-[300px]">
-            Plura
+          <h1 className="text-9xl font-bold text-center md:text-[100px] text-[50px]">
+            WebCraftPro
           </h1>
         </div>
-        <div className="flex justify-center items-center relative md:mt-[-70px]">
+        <div className="flex justify-center items-center relative md:mt-[-20px] ml-2 mr-2">
           <Image
             src={"/assets/preview.png"}
-            alt="banner image"
+            alt="preview"
             height={1200}
             width={1200}
-            className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted"
+            className="rounded-tl-2xl rounded-tr-2xl border-2 border-muted hei"
           />
           <div className="bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10"></div>
         </div>
       </section>
-      <section className="flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px]">
-        <h2 className="text-4xl text-center"> Choose what fits you right</h2>
+      <section className="flex justify-center items-center flex-col gap-4 md:mt-20 mt-[40px]">
+        <h2 className="text-4xl text-center">Choose what fits you right</h2>
         <p className="text-muted-foreground text-center">
-          Our straightforward pricing plans are tailored to meet your needs. If
-          {" you're"} not <br />
-          ready to commit you can get started for free.
+          Unlock unbeatable value with our website, enjoy premium features
+          designed to elevate your online presence. <br />
+          {"Don't"} miss out on our limited-time offer to power your business
+          with the best!
         </p>
         <div className="flex  justify-center gap-4 flex-wrap mt-6">
           {prices.data.map((card) => (
